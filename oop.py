5fec5bb0f170810bc38248ec789bead256a6bef5
John-Mukhwana <johnbradill67@gmail.com>
2024-12-16 15:37:01 +0300

    initialzed a constructor

--- oop.py
+++ oop.py
@@ -2,3 +2,8 @@
 # Designing a smartphone class
 
 class Smartphone:
+    def __init__ (self, brand, model,price):
+        self.brand= brand
+        self.model= model
+        self.price= price
+        
