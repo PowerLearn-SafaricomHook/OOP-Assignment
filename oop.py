9c8187693162ee337490ea7350f8c5a093a5b611
John-Mukhwana <johnbradill67@gmail.com>
2024-12-16 19:36:47 +0300

    Solved bug

--- oop.py
+++ oop.py
@@ -2,15 +2,15 @@
 # Designing a smartphone class
 
 class Smartphone:
-    def __init__ (self, brand, model,price):
-        self.brand= brand
-        self.model= model
-        self.price= price
+    def __init__ (self, brand, model, price):
+        self.brand = brand
+        self.model = model
+        self.price = price
         
-        def make_call(self, number):
+    def make_call(self, number):
             return f"Dialing {number} using {self.brand} {self.model}."
         
-        def send_message(self, number , message):
+    def send_message(self, number , message):
             return f"Sending message to {number}: {message}"
         
 # Devired class/ Subclass/ Child class/
