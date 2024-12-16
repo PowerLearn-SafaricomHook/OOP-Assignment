c15cf0bdda78711f39b7a2f72fa8c3e12b992d6d
John-Mukhwana <johnbradill67@gmail.com>
2024-12-16 22:15:04 +0300

    Add print statement to display animal movement

--- oop.py
+++ oop.py
@@ -70,4 +70,5 @@ animals = [Bird(), Fish(), Mammal()]
 # loop through the list of animals
 # in is used to iterate through the list
 for animal in animals:
+     print(f"{animal.__class__.__name__} is {animal.move()}.")
 
